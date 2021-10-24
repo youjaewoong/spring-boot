f2bf08b754c69bdd800207222169367bc241558c
gos1004 <nnv711@gmail.com>
2021-10-25 00:03:24 +0900

    회원 서비스 테스트

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,12 +3,14 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import lombok.AllArgsConstructor;
 
 import java.util.List;import java.util.Optional;
 
+@AllArgsConstructor
 public class MemberService {
 	
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	private final MemberRepository memberRepository;
  
 	/**
 	* 회원가입
