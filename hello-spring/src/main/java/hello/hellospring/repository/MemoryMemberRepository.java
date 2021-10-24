592e7259c1681c33e96b19ede88519f98aa0e3a2
gos1004 <nnv711@gmail.com>
2021-10-25 00:34:51 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,11 +2,14 @@ package hello.hellospring.repository;
 
 import java.util.*;
 
+import org.springframework.stereotype.Repository;
+
 import hello.hellospring.domain.Member;
 
 /**
  * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
  */
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
 	private static Map<Long, Member> store = new HashMap<>();
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -7,11 +7,19 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;import java.util.Optional;
 
-@AllArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
 	
 	private final MemberRepository memberRepository;
- 
+	
+	@Autowired
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
 	/**
 	* 회원가입
 	*/
