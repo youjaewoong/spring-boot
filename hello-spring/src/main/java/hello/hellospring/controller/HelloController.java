9bb0afa433fd6783225a59e2e579990dc5df09eb
gos1004 <nnv711@gmail.com>
2021-10-24 22:40:50 +0900

    API  - ResponseBody 사용원리

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Controller
 public class HelloController {
@@ -18,5 +22,25 @@ public class HelloController {
 	 public String helloMvc(@RequestParam("name") String name, Model model) {
 		 model.addAttribute("name", name);
 		 return "hello-template";
+	 }
+	 
+	 @GetMapping("hello-string")
+	 @ResponseBody
+	 public String helloString(@RequestParam("name") String name) {
+		 return "hello " + name;
+	 }
+	 
+	 @GetMapping("hello-api")
+	 @ResponseBody
+	 public Hello helloApi(@RequestParam("name") String name) {
+		 Hello hello = new Hello();
+		 hello.setName(name);
+		 return hello;
+	 }
+	 
+	 @Getter
+	 @Setter
+	 static class Hello {
+		 private String name;
 	 }
 }
