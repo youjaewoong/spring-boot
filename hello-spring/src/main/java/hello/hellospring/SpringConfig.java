7d88871fe3866ff2093b6cd2967514a2a0dbd6f0
gos1004 <nnv711@gmail.com>
2021-10-26 00:21:32 +0900

    순수 JDBC 적용

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig { 
+	
+	private final DataSource dataSource;
+	
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	
 	@Bean
 	public MemberService memberService() {
@@ -16,6 +26,8 @@ public class SpringConfig {
 	
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		//return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
+	
 }
